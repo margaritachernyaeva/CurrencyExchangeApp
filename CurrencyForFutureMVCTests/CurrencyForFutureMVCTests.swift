5f4ae5365c91ba05fd036b87173f3f838160d42c
ChernyaevaMargarita <chernyaeva760@yandex.by>
2020-11-05 16:42:02 +0300

    Initial Commit

---
+++ CurrencyForFutureMVCTests/CurrencyForFutureMVCTests.swift
@@ -0,0 +1,33 @@
+//
+//  CurrencyForFutureMVCTests.swift
+//  CurrencyForFutureMVCTests
+//
+//  Created by Маргарита Черняева on 11/5/20.
+//
+
+import XCTest
+@testable import CurrencyForFutureMVC
+
+class CurrencyForFutureMVCTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
